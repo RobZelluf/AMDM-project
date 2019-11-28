6d1210571f2fb8f83a9174fda40aa1b054596ec1
Dan Hopkins <dhopkinsnh@gmail.com>
2019-11-28 11:18:01 +0200

    Remove adjacency functionality from utils

--- crawler.py
+++ crawler.py
@@ -1,35 +1,45 @@
 from utils import read_file, write_file, Graph, score_partitioning
+from random_partitioning import random_partition
+
 import numpy as np
 import random
 import datetime
 
 graph = read_file('ca-GrQc.txt')
 
-def one_crawl(graph, steps):
+partitions = np.zeros(graph.num_vertices)
+
+
+def one_crawl(graph, v, steps):
     vertices_reached = list()
-    index = np.random.randint(0, graph.num_vertices)
     for i in range(steps):
-        vertices_reached.append(index)
-        index = graph.get_connected_vertex(index)
-    x = max(set(vertices_reached), key=vertices_reached.count)
+        vertices_reached.append(v)
+        v = graph.get_connected_vertex(v)
     return vertices_reached
 
 
-def many_crawls(graph, steps, crawls):
-    co_visit_matrix = np.zeros((graph.num_vertices, graph.num_vertices))
+def create_partition(graph, v, steps, crawls):
+    partition = list()
     for r in range(crawls):
-        vertices_reached = one_crawl(graph, steps)
-        for i in range(len(vertices_reached)):
-            for j in range(i+1, len(vertices_reached)):
-                if vertices_reached[i] != vertices_reached[j]:
-                    co_visit_matrix[vertices_reached[i]][vertices_reached[j]] += 1
-                    co_visit_matrix[vertices_reached[j]][vertices_reached[i]] += 1
-    return co_visit_matrix
+        crawl = one_crawl(graph, v, steps)
+        v = max(set(crawl), key=crawl.count)
+        partition.append(v)
+    return partition
 
 
 start = datetime.datetime.now()
-co_visit_matrix = many_crawls(graph, 100, 1000)
-print(co_visit_matrix[0, :])
+#
+# init_vertex = 0
+# p = partitions[init_vertex]
+# while p != 0:
+#     init_vertex += 1
+#     p = partitions[init_vertex]
+
+partition = create_partition(graph, 0, 100, 100)
+print(partition)
+print(len(set(partition)))
+
+
 end = datetime.datetime.now()
 print("Crawled in ", (end - start).total_seconds(), "seconds")
 
