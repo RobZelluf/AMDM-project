c967a6e184f01580a30ccc29e60380d7ad14230c
robzelluf <robzelluf@hotmail.com>
2019-11-28 10:49:51 +0200

    Update random partition

--- random_partitioning.py
+++ random_partitioning.py
@@ -3,10 +3,6 @@ import pandas as pd
 import pickle
 from utils import read_file, Graph, score_partitioning
 
-filename = 'CA-GrQc.txt'
-
-graph = read_file(filename)
-
 
 def random_partition(graph, k):
     partitions = np.zeros(graph.num_vertices)
@@ -14,9 +10,3 @@ def random_partition(graph, k):
         partitions[vertex] = int(np.random.randint(0, k - 1))
 
     return partitions
-
-
-partitions = random_partition(graph, 5)
-score = score_partitioning(graph, partitions)
-
-print("Score:", score)
