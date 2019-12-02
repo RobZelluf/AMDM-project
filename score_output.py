b8452145ef79d3262b0e3152c1b50ba15e34841e
robzelluf <robzelluf@hotmail.com>
2019-12-02 16:04:36 +0200

    Score ouputs

---
+++ score_output.py
@@ -0,0 +1,38 @@
+from utils import read_file, write_file, Graph, score_partitioning, random_partition, partition_count
+import numpy as np
+import datetime
+from crawl_utils import *
+import pickle
+import os
+import difflib
+
+# Input prompt for filename
+DIRs = [x for x in os.listdir("outputs/")]
+i = 0
+for DIR in DIRs:
+    print(i, DIR)
+    i += 1
+
+output_num = int(input("Output number:"))
+output_name = DIRs[output_num]
+filename_output = "outputs/" + output_name
+
+# Input prompt for filename
+DIRs = [x for x in os.listdir("data/")]
+
+data_name = difflib.get_close_matches(output_name, DIRs)[0]
+graph, _ = read_file(data_name)
+
+partitioning = []
+with open(filename_output, "r") as f:
+    for line in f.readlines():
+        if line[0] == "#" or line[0] == " ":
+            continue
+
+        line = line.split(" ")
+        partitioning.append(int(line[1]))
+
+print("Score:", score_partitioning(graph, partitioning))
+
+
+
