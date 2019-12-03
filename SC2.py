935d53a855044ffdd26be9915954c0bf373d23d1
Dan Hopkins <dhopkinsnh@gmail.com>
2019-12-03 09:32:27 +0200

    Fix parenthesis

--- SC2.py
+++ SC2.py
@@ -1,25 +1,26 @@
 from utils import *
+import numpy as np
 from sklearn.cluster import KMeans
 
-
-def reassign_partitions(graph, partitioning, k, min_score):
-    repartitioning = partitioning.copy()
-    for loops in range(3):
-        reassigned = 0
-        for v1 in range(graph.num_vertices):
-            votes = [0] * k
-            for v2 in graph.edge_dict[v1]:
-                votes[partitioning[v2]] += 1
-            if votes.index(max(votes)) != repartitioning[v1]:
-                reassigned += 1
-            repartitioning[v1] = votes.index(max(votes))
-        score = score_partitioning(graph,repartitioning)
-        if score < min_score:
-            min_score = score
-            partitioning = repartitioning.copy()
-        print("Reassigned: ", reassigned)
-        print(score)
-    return partitioning
+#
+# def reassign_partitions(graph, partitioning, k, min_score):
+#     repartitioning = partitioning.copy()
+#     for loops in range(3):
+#         reassigned = 0
+#         for v1 in range(graph.num_vertices):
+#             votes = [0] * k
+#             for v2 in graph.edge_dict[v1]:
+#                 votes[partitioning[v2]] += 1
+#             if votes.index(max(votes)) != repartitioning[v1]:
+#                 reassigned += 1
+#             repartitioning[v1] = votes.index(max(votes))
+#         score = score_partitioning(graph,repartitioning)
+#         if score < min_score:
+#             min_score = score
+#             partitioning = repartitioning.copy()
+#         print("Reassigned: ", reassigned)
+#         print(score)
+#     return partitioning
 
 filename = "Oregon-1.txt"
 graph, num_partitions = read_file(filename)
@@ -29,7 +30,7 @@ laplacian = graph.laplacian
 min_score = 1
 
 while min_score > 0.61:
-    vals, vecs = np.linalg.eigh(graph.laplacian))
+    vals, vecs = np.linalg.eigh(graph.laplacian)
     vecs = vecs[:, np.argsort(vals)]
     vals = vals[np.argsort(vals)]
     # kmeans on first three vectors with nonzero eigenvalues
