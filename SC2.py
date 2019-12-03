aa192ac2119ecbca5ecc6c3b6c9e0a33de4b5067
Dan Hopkins <dhopkinsnh@gmail.com>
2019-12-03 11:33:24 +0200

    Add reassign_partitions function

--- SC2.py
+++ SC2.py
@@ -2,26 +2,6 @@ from utils import *
 from sklearn.cluster import KMeans
 import numpy as np
 
-#
-# def reassign_partitions(graph, partitioning, k, min_score):
-#     repartitioning = partitioning.copy()
-#     for loops in range(3):
-#         reassigned = 0
-#         for v1 in range(graph.num_vertices):
-#             votes = [0] * k
-#             for v2 in graph.edge_dict[v1]:
-#                 votes[partitioning[v2]] += 1
-#             if votes.index(max(votes)) != repartitioning[v1]:
-#                 reassigned += 1
-#             repartitioning[v1] = votes.index(max(votes))
-#         score = score_partitioning(graph,repartitioning)
-#         if score < min_score:
-#             min_score = score
-#             partitioning = repartitioning.copy()
-#         print("Reassigned: ", reassigned)
-#         print(score)
-#     return partitioning
-
 filename = "Oregon-1.txt"
 graph, num_partitions = read_file(filename)
 laplacian = graph.laplacian
@@ -30,7 +10,7 @@ laplacian = graph.laplacian
 min_score = 1
 
 while min_score > 0.61:
-    vals, vecs = np.linalg.eig(graph.laplacian)
+    vals, vecs = np.linalg.eigh(graph.laplacian)
     vecs = vecs[:, np.argsort(vals)]
     vals = vals[np.argsort(vals)]
     # kmeans on first three vectors with nonzero eigenvalues
--- crawl_utils.py
+++ crawl_utils.py
@@ -111,3 +111,23 @@ def assign_partition(graph, partitioning, normalization, assign_v, num_partition
         return normalized_votes.argmax()
     else:
         return -1
+
+
+def reassign_partitions(graph, partitioning, k, min_score):
+    repartitioning = partitioning.copy()
+    for loops in range(3):
+        reassigned = 0
+        for v1 in range(graph.num_vertices):
+            votes = [0] * k
+            for v2 in graph.edge_dict[v1]:
+                votes[partitioning[v2]] += 1
+            if votes.index(max(votes)) != repartitioning[v1]:
+                reassigned += 1
+            repartitioning[v1] = votes.index(max(votes))
+        score = score_partitioning(graph,repartitioning)
+        if score < min_score:
+            min_score = score
+            partitioning = repartitioning.copy()
+        print("Reassigned: ", reassigned)
+        print(score)
+    return partitioning
